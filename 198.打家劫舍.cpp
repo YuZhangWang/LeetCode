391e3c17b6ac6e9988315cdd115c3444d9b21f0e
YuZhangWang <2277598524@qq.com>
2021-10-04 03:07:45 +0800

    update

--- 198.打家劫舍.cpp
+++ 198.打家劫舍.cpp
@@ -5,22 +5,17 @@
  */
 
 // @lc code=start
-class Solution
-{
+class Solution {
 public:
-    int rob(vector<int> &nums)
-    {
+    int rob(vector<int> &nums) {
         int length = nums.size();
         if (length == 0)
             return 0;
         if (length == 1)
             return nums[0];
-
         int first = nums[0];
         int second = max(nums[0], nums[1]);
-
-        for (int i = 2; i < length; i++)
-        {
+        for (int i = 2; i < length; i++) {
             int temp = second;
             second = max(second, first + nums[i]);
             first = temp;
@@ -29,3 +24,4 @@ public:
     }
 };
 // @lc code=end
+
